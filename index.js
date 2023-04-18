5453a7e12472fc97c3ee74b957986e51ec89cc87
AntonioValentin <cesar_anto30@hotmail.com>
2023-04-17 19:35:24 -0600

    Initial Commit 170423

---
+++ index.js
@@ -0,0 +1,196 @@
+let preguntas_aleatorias = true;
+let mostrar_pantalla_juego_terminado = true;
+let reiniciar_puntos_juego = true;
+
+/* window.onload = function ()
+{
+    basePreguntas = readText("base_preguntas.json");
+    interpreset_bp = JSON.parse(basePreguntas);
+    escogerPregunta();
+}; */
+
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'base_preguntas.json', true);
+xhr.onload = function () {
+  if (xhr.status === 200) {
+    var datos = JSON.parse(xhr.responseText);
+    // hacer algo con los datos
+    escogerPregunta();
+  } else {
+    console.log('Error al cargar el archivo:', xhr.status);
+  }
+};
+xhr.send();
+
+
+  
+let pregunta;
+let posiblesRespuestas;
+
+btn_correspondiente = 
+[
+    select_id("btn1"),
+    select_id("btn2"),
+    select_id("btn3"),
+    select_id("btn4")
+];
+let ngpreguntas = [];
+
+let preguntasHechas = 0;
+let preguntasCorrectas = 0;
+
+function escogerPregunta()
+{
+    let n;
+    if (preguntas_aleatorias)
+    {
+        n = Math.floor(Match.randoom() * interpreset_bp.length);    
+    } else
+    {n = 0;}
+    
+    while (ngpreguntas.includes(n))
+    {
+      n++;
+      if (n >= interpreset_bp.length)
+      {
+        n = 0;  
+      } 
+      //Aqui el Juego reinicia
+      if (ngpreguntas.length == interpreset_bp.length)
+      {
+        if(mostrar_pantalla_juego_terminado)
+        {
+            alert("Puntuacion: " + preguntasCorrectas + (preguntasHechas -1));
+        } 
+        if (reiniciar_puntos_juego)
+        {
+            preguntasCorrectas = 0;
+            preguntasHechas = 0;    
+        } 
+        ngpreguntas = [];
+      }
+    }
+    ngpreguntas.push(n);
+    preguntasHechas++;
+
+    escogerPregunta(n);
+}
+
+function escogerPregunta(n)
+{
+    pregunta = interpreset_bp[n];
+    select_id("categoria").innerHTML = pregunta.categoria;    
+    select_id("pregunta").innerHTML = pregunta.pregunta;    
+    select_id("numero").innerHTML = pregunta.n; 
+    
+    let pc = preguntasCorrectas;
+
+    if (preguntasHechas > 1)
+    {
+        select_id("puntaje").innerHTML = pc + " / " + (preguntasHechas - 1);
+    }else 
+    {
+        select_id("puntaje").innerHTML = " ";
+    }
+
+    style("imagen").objectFit = preguntas.objectFit;
+    desordenarRespuesta(pregunta);
+    if (pregunta.imagen)
+    {
+        select_id("imagen").setAttribute("src", pregunta.imagen);
+        style("imaggen").height = "200px";
+        style("imaggen").width = "100%";
+    }else {
+        style("imaggen").height = "0px";
+        style("imaggen").width = "0%";
+        setTimeout( () => {
+            select_id("imagen").setAttribute("src", "");
+        },500);
+    }
+}
+
+function desordenarRespuesta(pregunta)
+{
+    posibles_respuestas = 
+    [
+        pregunta.respuesta,
+        pregunta.incorrecta1,
+        pregunta.incorrecta2,
+        pregunta.incorrecta3,
+    ];
+    
+    posibles_respuestas.sort(() => Math.random() - 0.5);
+
+    select_id("btn1").innerHTML  = posibles_respuestas[0];
+    select_id("btn2").innerHTML  = posibles_respuestas[1];
+    select_id("btn3").innerHTML  = posibles_respuestas[2];
+    select_id("btn4").innerHTML  = posibles_respuestas[3];
+}
+
+let suspender_botones = false;
+
+function oprimir_btn(i)
+{
+    if (suspender_botones)
+    {
+        return;    
+    }    
+
+    suspender_botones = true;
+    if (posibles_respuestas[i] == pregunta.respuesta)
+    {
+        preguntasCorrectas++;    
+        btn_correspondiente[i].style.background = "lightgreen";
+    }else 
+    {
+        btn_correspondiente[i].style.background = "pink";
+    }
+
+    for (let j = 0; j < 4; j++)
+    {
+        if (posibles_respuestas[j] == pregunta.respuesta)
+        {
+            btn_correspondiente[i].style.background = "lightgreen";
+            break;
+        }    
+    }
+
+    setTimeout(() => {
+        reiniciar_puntos();
+        suspender_botones = false;
+    },3000);
+}
+
+function reiniciar_puntos()
+{
+    for(const btn of btn_correspondiente)
+    {
+        btn.style.basePreguntas="white";
+    }
+    escogerPreguntarAlatoria();
+}
+
+function select_id(id)
+{
+    return document.getElementById(id);    
+}
+
+function style(id)
+{
+    return select_id(id).style;    
+}
+
+function readText(ruta_local)
+{
+    var texto = null;
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open('GET', ruta_local, false);
+    xmlhttp.send();
+
+    if (xmlhttp.status == 200)
+    {
+        texto = xmlhttp.responseText;
+    }
+    return texto;
+}
+
