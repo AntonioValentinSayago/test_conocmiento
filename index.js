cee50df7b8adbe5628dce11685ebfad06fe88482
AntonioValentin <cesar_anto30@hotmail.com>
2023-04-17 21:33:35 -0600

    End Update 041723

--- index.js
+++ index.js
@@ -1,182 +1,153 @@
 let preguntas_aleatorias = true;
-let mostrar_pantalla_juego_terminado = true;
-let reiniciar_puntos_juego = true;
+let mostrar_pantalla_juego_términado = true;
+let reiniciar_puntos_al_reiniciar_el_juego = true;
 
-window.onload = function ()
-{
-    basePreguntas = readText("https://antoniovalentinsayago.github.io/test_conocmiento/base_preguntas.json");
-    interpreset_bp = JSON.parse(basePreguntas);
-    escogerPreguntaAleatoria();
+window.onload = function () {
+  base_preguntas = readText("https://antoniovalentinsayago.github.io/test_conocmiento/base_preguntas.json");
+  interprete_bp = JSON.parse(base_preguntas);
+  escogerPreguntaAleatoria();
 };
 
-function readText(ruta_local)
-{
-    var texto = null;
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.open('GET', ruta_local, false);
-    xmlhttp.send();
-
-    if (xmlhttp.status == 200)
-    {
-        texto = xmlhttp.responseText;
-    }
-    return texto;
-}
-  
 let pregunta;
-let posibles_espuestas;
-
-btn_correspondiente = 
-[
-    select_id("btn1"),
-    select_id("btn2"),
-    select_id("btn3"),
-    select_id("btn4")
+let posibles_respuestas;
+btn_correspondiente = [
+  select_id("btn1"),
+  select_id("btn2"),
+  select_id("btn3"),
+  select_id("btn4"),
 ];
-let ngpreguntas = [];
+let npreguntas = [];
 
-let preguntasHechas = 0;
-let preguntasCorrectas = 0;
+let preguntas_hechas = 0;
+let preguntas_correctas = 0;
 
-function escogerPreguntaAleatoria()
-{
-    let n;
-    if (preguntas_aleatorias)
-    {
-        n = Math.floor(Math.random() * interpreset_bp.length);    
-    } else
-    {n = 0;}
-    
-    while (ngpreguntas.includes(n))
-    {
-      n++;
-      if (n >= interpreset_bp.length)
-      {
-        n = 0;  
-      } 
-      //Aqui el Juego reinicia
-      if (ngpreguntas.length == interpreset_bp.length)
-      {
-        if(mostrar_pantalla_juego_terminado)
-        {
-            alert("Puntuacion: " + preguntasCorrectas + (preguntasHechas -1));
-        } 
-        if (reiniciar_puntos_juego)
-        {
-            preguntasCorrectas = 0;
-            preguntasHechas = 0;    
-        } 
-        ngpreguntas = [];
+function escogerPreguntaAleatoria() {
+  let n;
+  if (preguntas_aleatorias) {
+    n = Math.floor(Math.random() * interprete_bp.length);
+  } else {
+    n = 0;
+  }
+
+  while (npreguntas.includes(n)) {
+    n++;
+    if (n >= interprete_bp.length) {
+      n = 0;
+    }
+    if (npreguntas.length == interprete_bp.length) {
+      //Aquí es donde el juego se reinicia
+      if (mostrar_pantalla_juego_términado) {
+        swal.fire({
+          title: "Juego finalizado",
+          text:
+            "Puntuación: " + preguntas_correctas + "/" + (preguntas_hechas - 1),
+          icon: "success",
+        });
       }
+      if (reiniciar_puntos_al_reiniciar_el_juego) {
+        preguntas_correctas = 0;
+        preguntas_hechas = 0;
+      }
+      npreguntas = [];
     }
-    ngpreguntas.push(n);
-    preguntasHechas++;
+  }
+  npreguntas.push(n);
+  preguntas_hechas++;
 
-    escogerPregunta(n);
+  escogerPregunta(n);
 }
 
-function escogerPregunta(n)
-{
-    pregunta = interpreset_bp[n];
-    select_id("categoria").innerHTML = pregunta.categoria;    
-    select_id("pregunta").innerHTML = pregunta.pregunta;    
-    select_id("numero").innerHTML = pregunta.n; 
-    
-    let pc = preguntasCorrectas;
+function escogerPregunta(n) {
+  pregunta = interprete_bp[n];
+  select_id("categoria").innerHTML = pregunta.categoria;
+  select_id("pregunta").innerHTML = pregunta.pregunta;
+  select_id("numero").innerHTML = n;
+  let pc = preguntas_correctas;
+  if (preguntas_hechas > 1) {
+    select_id("puntaje").innerHTML = pc + "/" + (preguntas_hechas - 1);
+  } else {
+    select_id("puntaje").innerHTML = "";
+  }
 
-    if (preguntasHechas > 1)
-    {
-        select_id("puntaje").innerHTML = pc + " / " + (preguntasHechas - 1);
-    }else 
-    {
-        select_id("puntaje").innerHTML = " ";
-    }
-
-    style("imagen").objectFit = pregunta.objectFit;
-    desordenarRespuesta(pregunta);
-    if (pregunta.imagen)
-    {
-        select_id("imagen").setAttribute("src", pregunta.imagen);
-        style("imagen").height = "200px";
-        style("imagen").width = "100%";
-    }else {
-        style("imagen").height = "0px";
-        style("imagen").width = "0%";
-        setTimeout( () => {
-            select_id("imagen").setAttribute("src", "");
-        },500);
-    }
+  style("imagen").objectFit = pregunta.objectFit;
+  desordenarRespuestas(pregunta);
+  if (pregunta.imagen) {
+    select_id("imagen").setAttribute("src", pregunta.imagen);
+    style("imagen").height = "100px";
+    style("imagen").width = "100px";
+  } else {
+    style("imagen").height = "0px";
+    style("imagen").width = "0px";
+    setTimeout(() => {
+      select_id("imagen").setAttribute("src", "");
+    }, 500);
+  }
 }
 
-function desordenarRespuesta(pregunta)
-{
-    posibles_respuestas = 
-    [
-        pregunta.respuesta,
-        pregunta.incorrecta1,
-        pregunta.incorrecta2,
-        pregunta.incorrecta3,
-    ];
-    
-    posibles_respuestas.sort(() => Math.random() - 0.5);
+function desordenarRespuestas(pregunta) {
+  posibles_respuestas = [
+    pregunta.respuesta,
+    pregunta.incorrecta1,
+    pregunta.incorrecta2,
+    pregunta.incorrecta3,
+  ];
+  posibles_respuestas.sort(() => Math.random() - 0.5);
 
-    select_id("btn1").innerHTML  = posibles_respuestas[0];
-    select_id("btn2").innerHTML  = posibles_respuestas[1];
-    select_id("btn3").innerHTML  = posibles_respuestas[2];
-    select_id("btn4").innerHTML  = posibles_respuestas[3];
+  select_id("btn1").innerHTML = posibles_respuestas[0];
+  select_id("btn2").innerHTML = posibles_respuestas[1];
+  select_id("btn3").innerHTML = posibles_respuestas[2];
+  select_id("btn4").innerHTML = posibles_respuestas[3];
 }
 
 let suspender_botones = false;
 
-function oprimir_btn(i)
-{
-    if (suspender_botones)
-    {
-        return;    
-    }    
-
-    suspender_botones = true;
-    if (posibles_respuestas[i] == pregunta.respuesta)
-    {
-        preguntasCorrectas++;    
-        btn_correspondiente[i].style.background = "lightgreen";
-    }else 
-    {
-        btn_correspondiente[i].style.background = "pink";
+function oprimir_btn(i) {
+  if (suspender_botones) {
+    return;
+  }
+  suspender_botones = true;
+  if (posibles_respuestas[i] == pregunta.respuesta) {
+    preguntas_correctas++;
+    btn_correspondiente[i].style.background = "lightgreen";
+  } else {
+    btn_correspondiente[i].style.background = "pink";
+  }
+  for (let j = 0; j < 4; j++) {
+    if (posibles_respuestas[j] == pregunta.respuesta) {
+      btn_correspondiente[j].style.background = "lightgreen";
+      break;
     }
-
-    for (let j = 0; j < 4; j++)
-    {
-        if (posibles_respuestas[j] == pregunta.respuesta)
-        {
-            btn_correspondiente[i].style.background = "lightgreen";
-            break;
-        }    
-    }
-
-    setTimeout(() => {
-        reiniciar_puntos();
-        suspender_botones = false;
-    },3000);
+  }
+  setTimeout(() => {
+    reiniciar();
+    suspender_botones = false;
+  }, 3000);
 }
 
-function reiniciar_puntos()
-{
-    for(const btn of btn_correspondiente)
-    {
-        btn.style.background = "white";
-    }
-    escogerPreguntaAleatoria();
+// let p = prompt("numero")
+
+function reiniciar() {
+  for (const btn of btn_correspondiente) {
+    btn.style.background = "white";
+  }
+  escogerPreguntaAleatoria();
 }
 
-function select_id(id)
-{
-    return document.getElementById(id);    
+function select_id(id) {
+  return document.getElementById(id);
 }
 
-function style(id)
-{
-    return select_id(id).style;    
+function style(id) {
+  return select_id(id).style;
 }
 
-
+function readText(ruta_local) {
+  var texto = null;
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.open("GET", ruta_local, false);
+  xmlhttp.send();
+  if (xmlhttp.status == 200) {
+    texto = xmlhttp.responseText;
+  }
+  return texto;
+}
